d32c6c432fa056db7c41147596fcb7076f1d2e3c
s22026 <s22026@std.it-college.ac.jp>
2023-08-08 17:32:53 +0900

    7章終わり

---
+++ LifeCycleSample/app/src/main/java/jp/ac/it_college/std/s22026/lifecyclesample/SubActivity2.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s22026.lifecyclesample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub2)
+    }
+}
